46643464dac62ccdbd41746e4a8f3c1a4014b1da
monleru <kucherenko.alexandr1998@gmail.com>
2025-08-05 22:14:25 +0300

    .

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,6 +1,6 @@
 export const CONTRACT_ADDRESS = (import.meta as any).env?.VITE_CONTRACT_ADDRESS || 'ETUeYQcGuJE5oGBUKL2b55GPYE9SF5GFE2uTy2BRbonk';
 export const API_URL = (import.meta as any).env?.VITE_API_URL || 'http://localhost:3000';
 export const NETWORK = 'Solana';
-export const DEX_SCREENER_URL = 'https://letsbonk.fun/token/';
+export const DEX_SCREENER_URL = 'https://letsbonk.fun/token';
 export const TICKER = '$AILESS';
 export const X_URL = 'https://x.com/useless_AI_coin'; 
