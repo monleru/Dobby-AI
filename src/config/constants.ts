7cbc50cf891fda7a744fc0c2b13f2e6e1ac442fd
monleru <kucherenko.alexandr1998@gmail.com>
2025-08-05 22:08:43 +0300

    .

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const CONTRACT_ADDRESS = (import.meta as any).env?.VITE_CONTRACT_ADDRESS || '3gM3b64m7qyr1Xdkm9apeUMEMEQKJYQzYYTwwHGabonk';
+export const CONTRACT_ADDRESS = (import.meta as any).env?.VITE_CONTRACT_ADDRESS || 'ETUeYQcGuJE5oGBUKL2b55GPYE9SF5GFE2uTy2BRbonk';
 export const API_URL = (import.meta as any).env?.VITE_API_URL || 'http://localhost:3000';
 export const NETWORK = 'Solana';
 export const DEX_SCREENER_URL = 'https://letsbonk.fun/token/';
